28cb69600dab1f437d8a16978705e3d6544203bf
henrizx <marcelohenrizx@gmail.com>
2024-07-08 13:01:34 -0300

    inicio

---
+++ App01_CEP/App01_CEP/Servicos/ViaCEPServico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_CEP.Servicos.Modelo;
+using Newtonsoft.Json;
+namespace App01_CEP.Servicos
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "http://https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            return end;
+        }
+
+    }
+}
